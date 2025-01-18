84fb66f208735a8202b733913b6c64e53bcea77f
Apricot-S <mahjong_apricot@outlook.jp>
2025-01-18 22:45:54 +0000

    refactor: tweak `Action`

--- internal/message/action.go
+++ internal/message/action.go
@@ -1,6 +1,6 @@
 package message
 
 type Action struct {
-	Message
-	Actor int `json:"actor" validate:"min=0,max=3"`
+	Type  string `json:"type" validate:"required"`
+	Actor int    `json:"actor" validate:"min=0,max=3"`
 }
