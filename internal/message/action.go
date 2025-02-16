628ab0c5cf20e7d7e2419f22f67b3ec4e92a3c42
Apricot-S <mahjong_apricot@outlook.jp>
2025-02-16 08:53:35 +0000

    feat: Modify Action

--- internal/message/action.go
+++ internal/message/action.go
@@ -1,6 +1,6 @@
 package message
 
 type Action struct {
-	Type  string `json:"type" validate:"required"`
-	Actor int    `json:"actor" validate:"min=0,max=3"`
+	BaseMessage
+	Actor int `json:"actor" validate:"min=0,max=3"`
 }
