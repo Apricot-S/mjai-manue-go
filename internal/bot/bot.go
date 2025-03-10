4139741f58c0dfe228203ebfab904b7baaac6c7e
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-10 12:49:25 +0000

    feat: Implement Bot (WIP)

---
+++ internal/bot/bot.go
@@ -0,0 +1,17 @@
+package bot
+
+import (
+	"github.com/Apricot-S/mjai-manue-go/internal/ai"
+	"github.com/Apricot-S/mjai-manue-go/internal/game"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+type Bot struct {
+	state game.State
+	ai    ai.AI
+}
+
+func (b *Bot) Respond(message jsontext.Value) (jsontext.Value, error) {
+	// Dummy implementation
+	return []byte{}, nil
+}
