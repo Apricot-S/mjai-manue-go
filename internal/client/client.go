bf846a6246874742668354f21986128044f4dd1c
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-13 18:29:19 +0000

    feat: Implement Client (WIP)

--- internal/client/client.go
+++ internal/client/client.go
@@ -46,6 +46,28 @@ func (c *Client) Run() error {
 		}
 	case '[':
 		// array
+		var array []jsontext.Value
+		if err := json.Unmarshal(raw, &array); err != nil {
+			return fmt.Errorf("failed to unmarshal array: %w", err)
+		}
+
+		var lastRawRes jsontext.Value
+		for _, item := range array {
+			res, err := c.agent.Respond(&item)
+			if err != nil {
+				return err
+			}
+
+			rawRes, err := json.Marshal(&res)
+			if err != nil {
+				return fmt.Errorf("failed to marshal response: %w", err)
+			}
+			lastRawRes = rawRes
+		}
+
+		if _, err := c.writer.Write(lastRawRes); err != nil {
+			return fmt.Errorf("failed to write response: %w", err)
+		}
 	default:
 		return fmt.Errorf("invalid message: %v", raw)
 	}
