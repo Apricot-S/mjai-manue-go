faed9058b35616c14dc2cad41f1f061444ea05c4
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-13 12:08:42 +0000

    feat: Merge Client

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,5 +1,18 @@
 package client
 
-type Client interface {
-	Run() error
+import (
+	"io"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/agent"
+)
+
+type Client struct {
+	reader io.Reader
+	writer io.Writer
+	agent  *agent.Agent
+}
+
+func (c *Client) Run() error {
+	// Dummy
+	return nil
 }
