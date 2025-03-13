72946d0d4799dd49388aea774106027a06c1ec9e
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-13 17:58:57 +0000

    feat: Implement Client (WIP)

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,9 +1,12 @@
 package client
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/Apricot-S/mjai-manue-go/internal/agent"
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
 )
 
 type Client struct {
@@ -17,5 +20,19 @@ func NewClient(reader io.Reader, writer io.Writer, agent agent.Agent) *Client {
 }
 
 func (c *Client) Run() error {
+	var raw jsontext.Value
+	if err := json.UnmarshalRead(c.reader, raw); err != nil {
+		return fmt.Errorf("failed to read message: %w", err)
+	}
+
+	switch raw.Kind() {
+	case '{':
+		// single object
+	case '[':
+		// array
+	default:
+		return fmt.Errorf("invalid message: %v", raw)
+	}
+
 	return nil
 }
