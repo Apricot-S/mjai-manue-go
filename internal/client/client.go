18ddd29954e36521bee120ed650afcde154bbb20
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-14 11:00:16 +0000

    feat: Develop (WIP)

--- internal/client/client.go
+++ internal/client/client.go
@@ -21,56 +21,36 @@ func NewClient(reader io.Reader, writer io.Writer, agent agent.Agent) *Client {
 
 func (c *Client) Run() error {
 	var raw jsontext.Value
-	if err := json.UnmarshalRead(c.reader, &raw); err != nil {
-		if err == io.EOF {
-			return nil
-		}
-		return fmt.Errorf("failed to read message: %w", err)
-	}
 
-	switch raw.Kind() {
-	case '{':
-		// single object
-		res, err := c.agent.Respond(&raw)
+	for {
+		if err := json.UnmarshalRead(c.reader, &raw); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return fmt.Errorf("failed to read message: %w", err)
+		}
+
+		var msgs []jsontext.Value
+		switch raw.Kind() {
+		case '{':
+			// single object
+			msgs = []jsontext.Value{raw}
+		case '[':
+			// array
+			if err := json.Unmarshal(raw, &msgs); err != nil {
+				return fmt.Errorf("failed to unmarshal messages: %w", err)
+			}
+		default:
+			return fmt.Errorf("invalid message: %v", raw)
+		}
+
+		res, err := c.agent.Respond(msgs)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to respond from agent: %w", err)
 		}
 
-		rawRes, err := json.Marshal(&res)
-		if err != nil {
-			return fmt.Errorf("failed to marshal response: %w", err)
-		}
-
-		if _, err := c.writer.Write(rawRes); err != nil {
+		if _, err := c.writer.Write(res); err != nil {
 			return fmt.Errorf("failed to write response: %w", err)
 		}
-	case '[':
-		// array
-		var array []jsontext.Value
-		if err := json.Unmarshal(raw, &array); err != nil {
-			return fmt.Errorf("failed to unmarshal array: %w", err)
-		}
-
-		var lastRawRes jsontext.Value
-		for _, item := range array {
-			res, err := c.agent.Respond(&item)
-			if err != nil {
-				return err
-			}
-
-			rawRes, err := json.Marshal(&res)
-			if err != nil {
-				return fmt.Errorf("failed to marshal response: %w", err)
-			}
-			lastRawRes = rawRes
-		}
-
-		if _, err := c.writer.Write(lastRawRes); err != nil {
-			return fmt.Errorf("failed to write response: %w", err)
-		}
-	default:
-		return fmt.Errorf("invalid message: %v", raw)
 	}
-
-	return nil
 }
