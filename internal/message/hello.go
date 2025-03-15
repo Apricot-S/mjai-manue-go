81590569c8e10b1dc81c0b770a89dc0d123e63aa
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-15 23:30:56 +0000

    feat: Improve Hello

--- internal/message/hello.go
+++ internal/message/hello.go
@@ -1,15 +1,55 @@
 package message
 
+import (
+	"fmt"
+
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
 type Hello struct {
 	Message
 	Protocol        string `json:"protocol,omitempty"`
-	ProtocolVersion int    `json:"protocol_version,omitempty"`
+	ProtocolVersion int    `json:"protocol_version,omitzero" validate:"min=0"`
 }
 
-func NewHello(protocol string, protocolVersion int) *Hello {
-	return &Hello{
+func NewHello(protocol string, protocolVersion int) (*Hello, error) {
+	m := &Hello{
 		Message:         Message{Type: TypeHello},
 		Protocol:        protocol,
 		ProtocolVersion: protocolVersion,
 	}
+
+	if err := messageValidator.Struct(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (m *Hello) MarshalJSONTo(e *jsontext.Encoder, opts jsontext.Options) error {
+	if m.Type != TypeHello {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+	if err := messageValidator.Struct(m); err != nil {
+		return err
+	}
+
+	type inner Hello
+	mm := (inner)(*m)
+	return json.MarshalEncode(e, &mm)
+}
+
+func (m *Hello) UnmarshalJSONFrom(d *jsontext.Decoder, opts jsontext.Options) error {
+	type inner Hello
+	var mm inner
+	if err := json.UnmarshalDecode(d, &mm); err != nil {
+		return err
+	}
+
+	*m = (Hello)(mm)
+	if m.Type != TypeHello {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+
+	return messageValidator.Struct(m)
 }
--- internal/message/hello_test.go
+++ internal/message/hello_test.go
@@ -3,6 +3,8 @@ package message
 import (
 	"reflect"
 	"testing"
+
+	"github.com/go-json-experiment/json"
 )
 
 func TestNewHello(t *testing.T) {
@@ -15,6 +17,7 @@ func TestNewHello(t *testing.T) {
 		args     args
 		want     *Hello
 		wantJSON string
+		wantErr  bool
 	}{
 		{
 			name: "without protocol and protocol version",
@@ -26,6 +29,7 @@ func TestNewHello(t *testing.T) {
 				Message: Message{Type: TypeHello},
 			},
 			wantJSON: `{"type":"hello"}`,
+			wantErr:  false,
 		},
 		{
 			name: "with protocol and protocol version",
@@ -39,12 +43,129 @@ func TestNewHello(t *testing.T) {
 				ProtocolVersion: 1,
 			},
 			wantJSON: `{"type":"hello","protocol":"mjsonp","protocol_version":1}`,
+			wantErr:  false,
+		},
+		{
+			name: "invalid protocol version",
+			args: args{
+				protocol:        "mjsonp",
+				protocolVersion: -1,
+			},
+			want:     nil,
+			wantJSON: ``,
+			wantErr:  true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewHello(tt.args.protocol, tt.args.protocolVersion); !reflect.DeepEqual(got, tt.want) {
+			got, err := NewHello(tt.args.protocol, tt.args.protocolVersion)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewHello() error = %v, want %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewHello() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHello_Marshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    *Hello
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "without protocol and protocol version",
+			args: &Hello{
+				Message: Message{Type: TypeHello},
+			},
+			want:    `{"type":"hello"}`,
+			wantErr: false,
+		},
+		{
+			name: "with protocol and protocol version",
+			args: &Hello{
+				Message:         Message{Type: TypeHello},
+				Protocol:        "mjsonp",
+				ProtocolVersion: 1,
+			},
+			want:    `{"type":"hello","protocol":"mjsonp","protocol_version":1}`,
+			wantErr: false,
+		},
+		{
+			name: "invalid protocol version",
+			args: &Hello{
+				Message:         Message{Type: TypeHello},
+				Protocol:        "mjsonp",
+				ProtocolVersion: -1,
+			},
+			want:    ``,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := json.Marshal(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("marshal error = %v, want %v", err, tt.wantErr)
+			}
+			if string(b) != tt.want {
+				t.Errorf("Marshal() = %v, want %v", string(b), tt.want)
+			}
+		})
+	}
+}
+
+func TestHello_Unmarshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    string
+		want    Hello
+		wantErr bool
+	}{
+		{
+			name: "without protocol and protocol version",
+			args: `{"type":"hello"}`,
+			want: Hello{
+				Message:         Message{Type: TypeHello},
+				Protocol:        "",
+				ProtocolVersion: 0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "with protocol and protocol version",
+			args: `{"type":"hello","protocol":"mjsonp","protocol_version":1}`,
+			want: Hello{
+				Message:         Message{Type: TypeHello},
+				Protocol:        "mjsonp",
+				ProtocolVersion: 1,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid protocol version",
+			args: `{"type":"hello","protocol":"mjsonp","protocol_version":-1}`,
+			want: Hello{
+				Message:         Message{Type: TypeHello},
+				Protocol:        "mjsonp",
+				ProtocolVersion: -1,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got Hello
+			err := json.Unmarshal([]byte(tt.args), &got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("unmarshal error = %v, want %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Unmarshal() = %v, want %v", got, tt.want)
 			}
 		})
 	}
