799fb38546061943077cba5aa96f225e05b4e04e
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-16 08:39:36 +0000

    feat: Improve Furo (WIP)

--- internal/game/furo.go
+++ internal/game/furo.go
@@ -93,3 +93,197 @@ func (f *Furo) Target() *int {
 func (f *Furo) Pais() []Pai {
 	return f.pais
 }
+
+type FuroBase interface {
+	Taken() *Pai
+	Consumed() []Pai
+	Target() *int
+	Pais() []Pai
+}
+
+type FuroChi struct {
+	taken    Pai
+	consumed [2]Pai
+	target   int
+	pais     []Pai
+}
+
+func NewChi(taken Pai, consumed [2]Pai, target int) (*FuroChi, error) {
+	if target < 0 || target > 3 {
+		return nil, fmt.Errorf("chi: invalid target player index (must be 0-3, got: %d)", target)
+	}
+
+	var pais Pais = []Pai{taken, consumed[0], consumed[1]}
+	sort.Sort(pais)
+
+	return &FuroChi{
+		taken:    taken,
+		consumed: consumed,
+		target:   target,
+		pais:     pais,
+	}, nil
+}
+
+func (c *FuroChi) Taken() *Pai {
+	return &c.taken
+}
+
+func (c *FuroChi) Consumed() []Pai {
+	return c.consumed[:]
+}
+
+func (c *FuroChi) Target() *int {
+	return &c.target
+}
+
+func (c *FuroChi) Pais() []Pai {
+	return c.pais
+}
+
+type FuroPon struct {
+	taken    Pai
+	consumed [2]Pai
+	target   int
+	pais     []Pai
+}
+
+func NewPon(taken Pai, consumed [2]Pai, target int) (*FuroPon, error) {
+	if target < 0 || target > 3 {
+		return nil, fmt.Errorf("pon: invalid target player index (must be 0-3, got: %d)", target)
+	}
+
+	var pais Pais = []Pai{taken, consumed[0], consumed[1]}
+	sort.Sort(pais)
+
+	return &FuroPon{
+		taken:    taken,
+		consumed: consumed,
+		target:   target,
+		pais:     pais,
+	}, nil
+}
+
+func (p *FuroPon) Taken() *Pai {
+	return &p.taken
+}
+
+func (p *FuroPon) Consumed() []Pai {
+	return p.consumed[:]
+}
+
+func (p *FuroPon) Target() *int {
+	return &p.target
+}
+
+func (p *FuroPon) Pais() []Pai {
+	return p.pais
+}
+
+type FuroDaiminkan struct {
+	taken    Pai
+	consumed [3]Pai
+	target   int
+	pais     []Pai
+}
+
+func NewDaiminkan(taken Pai, consumed [3]Pai, target int) (*FuroDaiminkan, error) {
+	if target < 0 || target > 3 {
+		return nil, fmt.Errorf("daiminkan: invalid target player index (must be 0-3, got: %d)", target)
+	}
+
+	var pais Pais = []Pai{taken, consumed[0], consumed[1], consumed[2]}
+	sort.Sort(pais)
+
+	return &FuroDaiminkan{
+		taken:    taken,
+		consumed: consumed,
+		target:   target,
+		pais:     pais,
+	}, nil
+}
+
+func (d *FuroDaiminkan) Taken() *Pai {
+	return &d.taken
+}
+
+func (d *FuroDaiminkan) Consumed() []Pai {
+	return d.consumed[:]
+}
+
+func (d *FuroDaiminkan) Target() *int {
+	return &d.target
+}
+
+func (d *FuroDaiminkan) Pais() []Pai {
+	return d.pais
+}
+
+type FuroAnkan struct {
+	consumed [4]Pai
+	pais     []Pai
+}
+
+func NewAnkan(consumed [4]Pai) (*FuroAnkan, error) {
+	var pais Pais = []Pai{consumed[0], consumed[1], consumed[2], consumed[3]}
+	sort.Sort(pais)
+
+	return &FuroAnkan{
+		consumed: consumed,
+		pais:     pais,
+	}, nil
+}
+
+func (a *FuroAnkan) Taken() *Pai {
+	return nil
+}
+
+func (a *FuroAnkan) Consumed() []Pai {
+	return a.consumed[:]
+}
+
+func (a *FuroAnkan) Target() *int {
+	return nil
+}
+
+func (a *FuroAnkan) Pais() []Pai {
+	return a.pais
+}
+
+type FuroKakan struct {
+	taken    Pai
+	consumed [3]Pai
+	target   *int
+	pais     []Pai
+}
+
+func NewKakan(taken Pai, consumed [3]Pai, target *int) (*FuroKakan, error) {
+	if (target != nil) && (*target < 0 || *target > 3) {
+		return nil, fmt.Errorf("kakan: invalid target player index (must be 0-3, got: %d)", target)
+	}
+
+	var pais Pais = []Pai{taken, consumed[0], consumed[1], consumed[2]}
+	sort.Sort(pais)
+
+	return &FuroKakan{
+		taken:    taken,
+		consumed: consumed,
+		target:   target,
+		pais:     pais,
+	}, nil
+}
+
+func (k *FuroKakan) Taken() *Pai {
+	return &k.taken
+}
+
+func (k *FuroKakan) Consumed() []Pai {
+	return k.consumed[:]
+}
+
+func (k *FuroKakan) Target() *int {
+	return k.target
+}
+
+func (k *FuroKakan) Pais() []Pai {
+	return k.pais
+}
--- internal/game/furo_test.go
+++ internal/game/furo_test.go
@@ -1,199 +1,313 @@
 package game
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
-func TestNewFuro(t *testing.T) {
+func TestNewChi(t *testing.T) {
 	type args struct {
-		t        FuroType
-		taken    *Pai
-		consumed []Pai
+		taken    Pai
+		consumed [2]Pai
+		target   int
+	}
+	type testCase struct {
+		name    string
+		args    args
+		want    *FuroChi
+		wantErr bool
+	}
+	tests := []testCase{}
+
+	// valid cases
+	for _, strs := range [][2]string{{"1m", "2m 3m"}, {"1m", "3m 2m"}, {"5mr", "4m 6m"}} {
+		for i := range 4 {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+			want, _ := NewChi(*taken, [2]Pai(consumed), target)
+
+			testCase := testCase{
+				fmt.Sprintf("chi valid: target %d", target),
+				args{*taken, [2]Pai(consumed), target},
+				want,
+				false,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	// invalid cases
+	for _, strs := range [][2]string{{"1m", "2m 3m"}, {"1m", "3m 2m"}, {"5mr", "4m 6m"}} {
+		for _, i := range [2]int{-1, 4} {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+
+			testCase := testCase{
+				fmt.Sprintf("chi invalid: target %d", target),
+				args{*taken, [2]Pai(consumed), target},
+				nil,
+				true,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewChi(tt.args.taken, tt.args.consumed, tt.args.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewChi() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewChi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewPon(t *testing.T) {
+	type args struct {
+		taken    Pai
+		consumed [2]Pai
+		target   int
+	}
+	type testCase struct {
+		name    string
+		args    args
+		want    *FuroPon
+		wantErr bool
+	}
+	tests := []testCase{}
+
+	// valid cases
+	for _, strs := range [][2]string{{"E", "E E"}, {"5mr", "5m 5m"}, {"5p", "5pr 5p"}, {"5s", "5s 5sr"}} {
+		for i := range 4 {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+			want, _ := NewPon(*taken, [2]Pai(consumed), target)
+
+			testCase := testCase{
+				fmt.Sprintf("pon valid: target %d", target),
+				args{*taken, [2]Pai(consumed), target},
+				want,
+				false,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	// invalid cases
+	for _, strs := range [][2]string{{"E", "E E"}, {"5mr", "5m 5m"}, {"5p", "5pr 5p"}, {"5s", "5s 5sr"}} {
+		for _, i := range [2]int{-1, 4} {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+
+			testCase := testCase{
+				fmt.Sprintf("pon invalid: target %d", target),
+				args{*taken, [2]Pai(consumed), target},
+				nil,
+				true,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPon(tt.args.taken, tt.args.consumed, tt.args.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPon() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPon() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewDaiminkan(t *testing.T) {
+	type args struct {
+		taken    Pai
+		consumed [3]Pai
+		target   int
+	}
+	type testCase struct {
+		name    string
+		args    args
+		want    *FuroDaiminkan
+		wantErr bool
+	}
+	tests := []testCase{}
+
+	// valid cases
+	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
+		for i := range 4 {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+			want, _ := NewDaiminkan(*taken, [3]Pai(consumed), target)
+
+			testCase := testCase{
+				fmt.Sprintf("daiminkan valid: target %d", target),
+				args{*taken, [3]Pai(consumed), target},
+				want,
+				false,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	// invalid cases
+	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
+		for _, i := range [2]int{-1, 4} {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+
+			testCase := testCase{
+				fmt.Sprintf("daiminkan invalid: target %d", target),
+				args{*taken, [3]Pai(consumed), target},
+				nil,
+				true,
+			}
+			tests = append(tests, testCase)
+		}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewDaiminkan(tt.args.taken, tt.args.consumed, tt.args.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDaiminkan() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewDaiminkan() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewAnkan(t *testing.T) {
+	type args struct {
+		consumed [4]Pai
+	}
+	type testCase struct {
+		name    string
+		args    args
+		want    *FuroAnkan
+		wantErr bool
+	}
+	tests := []testCase{}
+
+	// valid cases
+	for _, strs := range []string{"E E E E", "5mr 5m 5m 5m", "5p 5pr 5p 5p", "5s 5s 5sr 5s", "5s 5s 5s 5sr"} {
+		consumed, _ := StrToPais(strs)
+		want, _ := NewAnkan([4]Pai(consumed))
+
+		testCase := testCase{
+			"ankan valid: target",
+			args{[4]Pai(consumed)},
+			want,
+			false,
+		}
+		tests = append(tests, testCase)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewAnkan(tt.args.consumed)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewAnkan() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewAnkan() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewKakan(t *testing.T) {
+	type args struct {
+		taken    Pai
+		consumed [3]Pai
 		target   *int
 	}
 	type testCase struct {
 		name    string
 		args    args
-		want    *Furo
+		want    *FuroKakan
 		wantErr bool
 	}
 	tests := []testCase{}
 
-	// Chi valid
-	for _, strs := range [][2]string{{"1m", "2m 3m"}, {"1m", "3m 2m"}, {"5mr", "4m 6m"}} {
-		taken, _ := NewPaiWithName(strs[0])
-		consumed, _ := StrToPais(strs[1])
-		target := 0
-		want, _ := NewFuro(Chi, taken, consumed, &target)
-		wantErr := false
-		tests = append(tests, testCase{"Chi", args{Chi, taken, consumed, &target}, want, wantErr})
+	// valid cases target: present
+	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
+		for i := range 4 {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
+			want, _ := NewKakan(*taken, [3]Pai(consumed), &target)
+
+			testCase := testCase{
+				fmt.Sprintf("kakan valid: target %d", target),
+				args{*taken, [3]Pai(consumed), &target},
+				want,
+				false,
+			}
+			tests = append(tests, testCase)
+		}
 	}
 
-	// Chi invalid
-	{
-		taken, _ := NewPaiWithName("1m")
-		consumed2, _ := StrToPais("2m 3m")
-		consumed1, _ := StrToPais("2m")
-		consumed3, _ := StrToPais("2m 3m 4m")
-		target := 0
-
-		tests = append(tests, testCase{"Chi invalid: taken: nil", args{Chi, nil, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: taken: nil, target: nil", args{Chi, nil, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: taken: nil, consumed: 1", args{Chi, nil, consumed1, &target}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: taken: nil, consumed: 1, target: nil", args{Chi, nil, consumed1, nil}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: taken: nil, consumed: 3", args{Chi, nil, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: taken: nil, consumed: 3, target: nil", args{Chi, nil, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: target: nil", args{Chi, taken, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: consumed: 1", args{Chi, taken, consumed1, &target}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: consumed: 1, target: nil", args{Chi, taken, consumed1, nil}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: consumed: 3", args{Chi, taken, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Chi invalid: consumed: 3, target: nil", args{Chi, taken, consumed3, nil}, nil, true})
-	}
-
-	// Pon valid
-	for _, strs := range [][2]string{{"E", "E E"}, {"5mr", "5m 5m"}, {"5p", "5pr 5p"}, {"5s", "5s 5sr"}} {
-		taken, _ := NewPaiWithName(strs[0])
-		consumed, _ := StrToPais(strs[1])
-		target := 1
-		want, _ := NewFuro(Pon, taken, consumed, &target)
-		wantErr := false
-		tests = append(tests, testCase{"Pon", args{Pon, taken, consumed, &target}, want, wantErr})
-	}
-
-	// Pon invalid
-	{
-		taken, _ := NewPaiWithName("1m")
-		consumed2, _ := StrToPais("1m 1m")
-		consumed1, _ := StrToPais("1m")
-		consumed3, _ := StrToPais("1m 1m 1m")
-		target := 0
-
-		tests = append(tests, testCase{"Pon invalid: taken: nil", args{Pon, nil, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: taken: nil, target: nil", args{Pon, nil, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: taken: nil, consumed: 1", args{Pon, nil, consumed1, &target}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: taken: nil, consumed: 1, target: nil", args{Pon, nil, consumed1, nil}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: taken: nil, consumed: 3", args{Pon, nil, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: taken: nil, consumed: 3, target: nil", args{Pon, nil, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: target: nil", args{Pon, taken, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: consumed: 1", args{Pon, taken, consumed1, &target}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: consumed: 1, target: nil", args{Pon, taken, consumed1, nil}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: consumed: 3", args{Pon, taken, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Pon invalid: consumed: 3, target: nil", args{Pon, taken, consumed3, nil}, nil, true})
-	}
-
-	// Daiminkan valid
+	// valid cases target: nil
 	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
 		taken, _ := NewPaiWithName(strs[0])
 		consumed, _ := StrToPais(strs[1])
-		target := 2
-		want, _ := NewFuro(Daiminkan, taken, consumed, &target)
-		wantErr := false
+		want, _ := NewKakan(*taken, [3]Pai(consumed), nil)
 
-		tests = append(tests, testCase{"Daiminkan", args{Daiminkan, taken, consumed, &target}, want, wantErr})
+		testCase := testCase{
+			"kakan valid: target nil",
+			args{*taken, [3]Pai(consumed), nil},
+			want,
+			false,
+		}
+		tests = append(tests, testCase)
 	}
 
-	// Daiminkan invalid
-	{
-		taken, _ := NewPaiWithName("1m")
-		consumed3, _ := StrToPais("1m 1m 1m")
-		consumed2, _ := StrToPais("1m 1m")
-		consumed4, _ := StrToPais("1m 1m 1m 1m")
-		target := 0
-
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil", args{Daiminkan, nil, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil, target: nil", args{Daiminkan, nil, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil, consumed: 2", args{Daiminkan, nil, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil, consumed: 2, target: nil", args{Daiminkan, nil, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil, consumed: 4", args{Daiminkan, nil, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: taken: nil, consumed: 4, target: nil", args{Daiminkan, nil, consumed4, nil}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: target: nil", args{Daiminkan, taken, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: consumed: 2", args{Daiminkan, taken, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: consumed: 2, target: nil", args{Daiminkan, taken, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: consumed: 4", args{Daiminkan, taken, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Daiminkan invalid: consumed: 4, target: nil", args{Daiminkan, taken, consumed4, nil}, nil, true})
-	}
-
-	// Ankan valid
-	for _, consumed := range []string{"E E E E", "5mr 5m 5m 5m", "5p 5pr 5p 5p", "5s 5s 5sr 5s", "5s 5s 5s 5sr"} {
-		consumed, _ := StrToPais(consumed)
-		want, _ := NewFuro(Ankan, nil, consumed, nil)
-		wantErr := false
-
-		tests = append(tests, testCase{"Ankan", args{Ankan, nil, consumed, nil}, want, wantErr})
-	}
-
-	// Ankan invalid
-	{
-		taken, _ := NewPaiWithName("1m")
-		consumed4, _ := StrToPais("1m 1m 1m 1m")
-		consumed3, _ := StrToPais("1m 1m 1m")
-		consumed5, _ := StrToPais("1m 1m 1m 1m 1m")
-		target := 0
-
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil, target: not nil", args{Ankan, taken, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil", args{Ankan, taken, consumed4, nil}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil, consumed: 3, target: not nil", args{Ankan, taken, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil, consumed: 3", args{Ankan, taken, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil, consumed: 5, target: not nil", args{Ankan, taken, consumed5, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: taken: not nil, consumed: 5", args{Ankan, taken, consumed5, nil}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: target: not nil", args{Ankan, nil, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: consumed: 3, target: not nil", args{Ankan, nil, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: consumed: 3", args{Ankan, nil, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: consumed: 5, target: not nil", args{Ankan, nil, consumed5, &target}, nil, true})
-		tests = append(tests, testCase{"Ankan invalid: consumed: 5", args{Ankan, nil, consumed5, nil}, nil, true})
-	}
-
-	// Kakan valid target: present
+	// invalid cases
 	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
-		taken, _ := NewPaiWithName(strs[0])
-		consumed, _ := StrToPais(strs[1])
-		target := 3
-		want, _ := NewFuro(Kakan, taken, consumed, &target)
-		wantErr := false
+		for _, i := range [2]int{-1, 4} {
+			taken, _ := NewPaiWithName(strs[0])
+			consumed, _ := StrToPais(strs[1])
+			target := i
 
-		tests = append(tests, testCase{"Kakan", args{Kakan, taken, consumed, &target}, want, wantErr})
-	}
-
-	// Kakan valid target: nil
-	for _, strs := range [][2]string{{"E", "E E E"}, {"5mr", "5m 5m 5m"}, {"5p", "5pr 5p 5p"}, {"5s", "5s 5sr 5s"}, {"5s", "5s 5s 5sr"}} {
-		taken, _ := NewPaiWithName(strs[0])
-		consumed, _ := StrToPais(strs[1])
-		want, _ := NewFuro(Kakan, taken, consumed, nil)
-		wantErr := false
-
-		tests = append(tests, testCase{"Kakan", args{Kakan, taken, consumed, nil}, want, wantErr})
-	}
-
-	// Kakan invalid
-	{
-		taken, _ := NewPaiWithName("1m")
-		consumed3, _ := StrToPais("1m 1m 1m")
-		consumed2, _ := StrToPais("1m 1m")
-		consumed4, _ := StrToPais("1m 1m 1m 1m")
-		target := 0
-
-		tests = append(tests, testCase{"Kakan invalid: taken: nil", args{Kakan, nil, consumed3, &target}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: taken: nil, target: nil", args{Kakan, nil, consumed3, nil}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: taken: nil, consumed: 2", args{Kakan, nil, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: taken: nil, consumed: 2, target: nil", args{Kakan, nil, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: taken: nil, consumed: 4", args{Kakan, nil, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: taken: nil, consumed: 4, target: nil", args{Kakan, nil, consumed4, nil}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: consumed: 2", args{Kakan, taken, consumed2, &target}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: consumed: 2, target: nil", args{Kakan, taken, consumed2, nil}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: consumed: 4", args{Kakan, taken, consumed4, &target}, nil, true})
-		tests = append(tests, testCase{"Kakan invalid: consumed: 4, target: nil", args{Kakan, taken, consumed4, nil}, nil, true})
+			testCase := testCase{
+				fmt.Sprintf("kakan invalid: target %d", target),
+				args{*taken, [3]Pai(consumed), &target},
+				nil,
+				true,
+			}
+			tests = append(tests, testCase)
+		}
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewFuro(tt.args.t, tt.args.taken, tt.args.consumed, tt.args.target)
+			got, err := NewKakan(tt.args.taken, tt.args.consumed, tt.args.target)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("NewFuro() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("NewKakan() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewFuro() = %v, want %v", got, tt.want)
+				t.Errorf("NewKakan() = %v, want %v", got, tt.want)
 			}
 		})
 	}
