560e83d6f6d3281afe7ab9e0dbba4a98c47ac73e
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-18 14:52:25 +0000

    refactor: Organize module definition locations

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,31 +1,7 @@
 package agent
 
-import (
-	"fmt"
-
-	"github.com/Apricot-S/mjai-manue-go/internal/message"
-	"github.com/go-json-experiment/json"
-	"github.com/go-json-experiment/json/jsontext"
-)
+import "github.com/go-json-experiment/json/jsontext"
 
 type Agent interface {
 	Respond(msgs []jsontext.Value) (jsontext.Value, error)
-}
-
-func makeNoneResponse() (jsontext.Value, error) {
-	none := message.NewNone()
-	res, err := json.Marshal(&none)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal none message: %w", err)
-	}
-	return res, nil
-}
-
-func makeJoinResponse(name string, room string) (jsontext.Value, error) {
-	join := message.NewJoin(name, room)
-	res, err := json.Marshal(&join)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal join message: %w", err)
-	}
-	return res, nil
 }
---
+++ internal/agent/common.go
@@ -0,0 +1,27 @@
+package agent
+
+import (
+	"fmt"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/message"
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+func makeNoneResponse() (jsontext.Value, error) {
+	none := message.NewNone()
+	res, err := json.Marshal(&none)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal none message: %w", err)
+	}
+	return res, nil
+}
+
+func makeJoinResponse(name string, room string) (jsontext.Value, error) {
+	join := message.NewJoin(name, room)
+	res, err := json.Marshal(&join)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal join message: %w", err)
+	}
+	return res, nil
+}
