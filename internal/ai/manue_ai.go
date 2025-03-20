6e90861e090a9e8117b87bfe0f9835d803aac3e2
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-20 21:31:05 +0000

    feat: Implement NewManueAI()

---
+++ internal/ai/manue_ai.go
@@ -0,0 +1,27 @@
+package ai
+
+import "github.com/Apricot-S/mjai-manue-go/configs"
+
+type ManueAI struct {
+	stats               *configs.GameStats
+	dangerEstimator     *DangerEstimator
+	tenpaiProbEstimator *TenpaiProbEstimator
+	noChanges           [4]int
+}
+
+func NewManueAI(stats *configs.GameStats, root *configs.DangerNode) *ManueAI {
+	return NewManueAIWithEstimators(
+		stats,
+		&DangerEstimator{root: root},
+		&TenpaiProbEstimator{stats: stats},
+	)
+}
+
+func NewManueAIWithEstimators(stats *configs.GameStats, dangerEstimator *DangerEstimator, tenpaiProbEstimator *TenpaiProbEstimator) *ManueAI {
+	return &ManueAI{
+		stats:               stats,
+		dangerEstimator:     dangerEstimator,
+		tenpaiProbEstimator: tenpaiProbEstimator,
+		noChanges:           [4]int{},
+	}
+}
