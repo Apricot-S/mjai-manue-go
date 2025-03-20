c8ba80e466cba2aab762b44b5398741effa9f48f
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-20 06:54:07 +0000

    feat: Implement Reach

---
+++ internal/message/reach.go
@@ -0,0 +1,55 @@
+package message
+
+import (
+	"fmt"
+
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+type Reach struct {
+	Action
+}
+
+func NewReach(actor int, log string) (*Reach, error) {
+	m := &Reach{
+		Action: Action{
+			Message: Message{Type: TypeReach},
+			Actor:   actor,
+			Log:     log,
+		},
+	}
+
+	if err := messageValidator.Struct(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (m *Reach) MarshalJSONTo(e *jsontext.Encoder, opts jsontext.Options) error {
+	if m.Type != TypeReach {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+	if err := messageValidator.Struct(m); err != nil {
+		return err
+	}
+
+	type inner Reach
+	mm := (inner)(*m)
+	return json.MarshalEncode(e, &mm)
+}
+
+func (m *Reach) UnmarshalJSONFrom(d *jsontext.Decoder, opts jsontext.Options) error {
+	type inner Reach
+	var mm inner
+	if err := json.UnmarshalDecode(d, &mm); err != nil {
+		return err
+	}
+
+	*m = (Reach)(mm)
+	if m.Type != TypeReach {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+
+	return messageValidator.Struct(m)
+}
