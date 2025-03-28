9247740d914a27a90f0ed8b86e72ffb8678febfc
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-28 23:50:51 +0000

    feat: Implement BitVector

---
+++ internal/ai/bit_vector.go
@@ -0,0 +1,23 @@
+package ai
+
+import "github.com/Apricot-S/mjai-manue-go/internal/game"
+
+type BitVector uint64
+
+func NewBitVector(countVector *[game.NumIDs]int, threshold int) BitVector {
+	var bv BitVector
+	for i, c := range countVector {
+		if c >= threshold {
+			bv |= BitVector(1) << i
+		}
+	}
+	return bv
+}
+
+func (bv BitVector) IsSubsetOf(other BitVector) bool {
+	return (bv & other) == bv
+}
+
+func (bv BitVector) HasIntersectionWith(other BitVector) bool {
+	return (bv & other) != 0
+}
