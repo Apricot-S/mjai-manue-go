bd30850d6ade0976b27abc402db37207fcf2049e
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-29 13:09:52 +0000

    refactor: Add NewAIAgentWithState

--- internal/agent/ai.go
+++ internal/agent/ai.go
@@ -16,13 +16,17 @@ type AIAgent struct {
 }
 
 func NewAIAgent(name string, room string, ai ai.AI) *AIAgent {
+	return NewAIAgentWithState(name, room, ai, &game.StateImpl{})
+}
+
+func NewAIAgentWithState(name string, room string, ai ai.AI, state game.State) *AIAgent {
 	return &AIAgent{
 		name:     name,
 		room:     room,
 		ai:       ai,
 		playerID: -1,
 		inGame:   false,
-		state:    nil,
+		state:    state,
 	}
 }
 
