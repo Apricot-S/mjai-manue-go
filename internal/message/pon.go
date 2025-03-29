269a99bc928324d8edf760c8b2de8ec0c5e3bc21
Apricot-S <mahjong_apricot@outlook.jp>
2025-03-29 07:52:43 +0000

    feat: Add Pon

---
+++ internal/message/pon.go
@@ -0,0 +1,61 @@
+package message
+
+import (
+	"fmt"
+
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+type Pon struct {
+	Action
+	Target   int       `json:"target" validate:"min=0,max=3"`
+	Pai      string    `json:"pai" validate:"tile"`
+	Consumed [2]string `json:"consumed" validate:"dive,tile"`
+}
+
+func NewPon(actor int, target int, pai string, consumed [2]string, log string) (*Pon, error) {
+	m := &Pon{
+		Action: Action{
+			Message: Message{Type: TypePon},
+			Actor:   actor,
+			Log:     log,
+		},
+		Target:   target,
+		Pai:      pai,
+		Consumed: consumed,
+	}
+
+	if err := messageValidator.Struct(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (m *Pon) MarshalJSONTo(e *jsontext.Encoder) error {
+	if m.Type != TypePon {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+	if err := messageValidator.Struct(m); err != nil {
+		return err
+	}
+
+	type inner Pon
+	mm := (inner)(*m)
+	return json.MarshalEncode(e, &mm)
+}
+
+func (m *Pon) UnmarshalJSONFrom(d *jsontext.Decoder) error {
+	type inner Pon
+	var mm inner
+	if err := json.UnmarshalDecode(d, &mm); err != nil {
+		return err
+	}
+
+	*m = (Pon)(mm)
+	if m.Type != TypePon {
+		return fmt.Errorf("invalid type: %v", m.Type)
+	}
+
+	return messageValidator.Struct(m)
+}
