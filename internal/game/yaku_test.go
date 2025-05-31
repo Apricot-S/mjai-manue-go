e7e7ad59c69dcf92c758146206d7b5b953a13404
Apricot-S <mahjong_apricot@outlook.jp>
2025-05-31 14:57:46 +0000

    test: Add unit tests

--- internal/game/yaku_test.go
+++ internal/game/yaku_test.go
@@ -49,3 +49,64 @@ func Test_isTanyaochu(t *testing.T) {
 		})
 	}
 }
+
+func Test_isChantaiyao(t *testing.T) {
+	type args struct {
+		allMentsus []Mentsu
+	}
+	type testCase struct {
+		name string
+		args args
+		want bool
+	}
+	tests := []testCase{}
+
+	{
+		name := "is chantaiyao"
+		mentsus := make([]Mentsu, 5)
+		pais1, _ := StrToPais("1m 2m 3m")
+		pais2, _ := StrToPais("7p 8p 9p")
+		pais3, _ := StrToPais("1s 1s 1s")
+		pais4, _ := StrToPais("E E E E")
+		pais5, _ := StrToPais("N N")
+		mentsus[0] = NewShuntsu(pais1[0], pais1[1], pais1[2])
+		mentsus[1] = NewShuntsu(pais2[0], pais2[1], pais2[2])
+		mentsus[2] = NewKotsu(pais3[0], pais3[1], pais3[2])
+		mentsus[3] = NewKantsu(pais4[0], pais4[1], pais4[2], pais4[3])
+		mentsus[4] = NewToitsu(pais5[0], pais5[1])
+
+		tests = append(tests, testCase{
+			name: name,
+			args: args{allMentsus: mentsus},
+			want: true,
+		})
+	}
+	{
+		name := "is not chantaiyao"
+		mentsus := make([]Mentsu, 5)
+		pais1, _ := StrToPais("2m 3m 4m")
+		pais2, _ := StrToPais("7p 8p 9p")
+		pais3, _ := StrToPais("1s 1s 1s")
+		pais4, _ := StrToPais("E E E E")
+		pais5, _ := StrToPais("N N")
+		mentsus[0] = NewShuntsu(pais1[0], pais1[1], pais1[2])
+		mentsus[1] = NewShuntsu(pais2[0], pais2[1], pais2[2])
+		mentsus[2] = NewKotsu(pais3[0], pais3[1], pais3[2])
+		mentsus[3] = NewKantsu(pais4[0], pais4[1], pais4[2], pais4[3])
+		mentsus[4] = NewToitsu(pais5[0], pais5[1])
+
+		tests = append(tests, testCase{
+			name: name,
+			args: args{allMentsus: mentsus},
+			want: false,
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isChantaiyao(tt.args.allMentsus); got != tt.want {
+				t.Errorf("isChantaiyao() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
