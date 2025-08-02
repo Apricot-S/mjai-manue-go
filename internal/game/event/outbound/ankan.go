ab1088b463d3785b1385d108fe3e8e38090c0932
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-02 16:39:44 +0000

    feat: Add Ankan

---
+++ internal/game/event/outbound/ankan.go
@@ -0,0 +1,35 @@
+package outbound
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
+
+type Ankan struct {
+	action
+	Consumed [4]base.Pai
+}
+
+func NewAnkan(actor int, consumed [4]base.Pai, log string) (*Ankan, error) {
+	event := &Ankan{
+		action: action{
+			Actor: actor,
+			Log:   log,
+		},
+		Consumed: consumed,
+	}
+
+	isValidPais := !slices.ContainsFunc(event.Consumed[:], func(p base.Pai) bool {
+		return !event.Consumed[0].HasSameSymbol(&p)
+	})
+	if !isValidPais {
+		return nil, fmt.Errorf("all consumed tiles must be the same tile: %v", event)
+	}
+
+	if err := eventValidator.Struct(event); err != nil {
+		return nil, err
+	}
+	return event, nil
+}
