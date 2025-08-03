099d68fd081460f6836dd03d8fd8e8f9bdf3c93e
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-03 01:43:34 +0000

    feat: Add Chi

---
+++ internal/game/event/outbound/chi.go
@@ -0,0 +1,63 @@
+package outbound
+
+import (
+	"fmt"
+	"slices"
+	"sort"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
+
+type Chi struct {
+	action
+	Target   int `validate:"min=0,max=3"`
+	Taken    base.Pai
+	Consumed [2]base.Pai
+}
+
+func NewChi(actor int, target int, taken base.Pai, consumed [2]base.Pai, log string) (*Chi, error) {
+	event := &Chi{
+		action: action{
+			Actor: actor,
+			Log:   log,
+		},
+		Target:   target,
+		Taken:    taken,
+		Consumed: consumed,
+	}
+
+	if getPlayerDistance(event.Actor, event.Target) != 1 {
+		return nil, fmt.Errorf("target must be the kamicha of actor: actor=%d, target=%d", actor, target)
+	}
+
+	isSuhai := !slices.ContainsFunc(event.Consumed[:], func(p base.Pai) bool {
+		return p.IsTsupai()
+	})
+	if !isSuhai {
+		return nil, fmt.Errorf("consumed tiles must not be tsupai: %v", event)
+	}
+
+	isSameColor := !slices.ContainsFunc(event.Consumed[:], func(p base.Pai) bool {
+		return event.Taken.Type() != p.Type()
+	})
+	if !isSameColor {
+		return nil, fmt.Errorf("taken tile must be the same color as the consumed tile: %v", event)
+	}
+
+	pais := slices.Concat(base.Pais{event.Taken}, event.Consumed[:])
+	sort.Sort(pais)
+	isSequence := pais[0].Number()+1 == pais[1].Number() && pais[1].Number()+1 == pais[2].Number()
+	if !isSequence {
+		return nil, fmt.Errorf("consumed tiles must be a sequence with the taken tile: %v", event)
+	}
+
+	if err := eventValidator.Struct(event); err != nil {
+		return nil, err
+	}
+	return event, nil
+}
+
+func getPlayerDistance(p1 int, p2 int) int {
+	const NumPlayers = 4
+	return (NumPlayers + p1 - p2) % NumPlayers
+}
---
+++ internal/game/event/outbound/chi_test.go
@@ -0,0 +1,182 @@
+package outbound
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
+
+func TestNewChi(t *testing.T) {
+	type args struct {
+		actor    int
+		target   int
+		taken    base.Pai
+		consumed [2]base.Pai
+		log      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Chi
+		wantErr bool
+	}{
+		{
+			name: "without log",
+			args: args{
+				actor:    1,
+				target:   0,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			want: &Chi{
+				action: action{
+					Actor: 1,
+					Log:   "",
+				},
+				Target:   0,
+				Taken:    *mustPai("6s"),
+				Consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "with log",
+			args: args{
+				actor:    3,
+				target:   2,
+				taken:    *mustPai("5sr"),
+				consumed: [2]base.Pai{*mustPai("4s"), *mustPai("6s")},
+				log:      "test",
+			},
+			want: &Chi{
+				action: action{
+					Actor: 3,
+					Log:   "test",
+				},
+				Target:   2,
+				Taken:    *mustPai("5sr"),
+				Consumed: [2]base.Pai{*mustPai("4s"), *mustPai("6s")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid actor min",
+			args: args{
+				actor:    -1,
+				target:   0,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid actor max",
+			args: args{
+				actor:    4,
+				target:   3,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid target min",
+			args: args{
+				actor:    0,
+				target:   -1,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid target max",
+			args: args{
+				actor:    0,
+				target:   4,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "target is not kamicha",
+			args: args{
+				actor:    0,
+				target:   1,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5sr"), *mustPai("7s")},
+				log:      "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "tsupai is not allowed",
+			args: args{
+				actor:    0,
+				target:   3,
+				taken:    *mustPai("E"),
+				consumed: [2]base.Pai{*mustPai("S"), *mustPai("W")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "different color is not allowed: taken is different from consumed",
+			args: args{
+				actor:    0,
+				target:   3,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("5p"), *mustPai("7p")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "different color is not allowed: consumed contains different color",
+			args: args{
+				actor:    0,
+				target:   3,
+				taken:    *mustPai("6p"),
+				consumed: [2]base.Pai{*mustPai("5p"), *mustPai("7s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid sequence: taken is not in sequence with consumed",
+			args: args{
+				actor:    0,
+				target:   3,
+				taken:    *mustPai("6p"),
+				consumed: [2]base.Pai{*mustPai("5p"), *mustPai("8p")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewChi(tt.args.actor, tt.args.target, tt.args.taken, tt.args.consumed, tt.args.log)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewChi() error = %v, want %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewChi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
