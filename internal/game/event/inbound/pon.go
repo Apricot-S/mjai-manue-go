4e85e97888798248ba2ebb932cbae14283044191
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-04 13:26:35 +0000

    feat: Add Pon

---
+++ internal/game/event/inbound/pon.go
@@ -0,0 +1,44 @@
+package inbound
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
+
+type Pon struct {
+	Actor    int `validate:"min=0,max=3"`
+	Target   int `validate:"min=0,max=3"`
+	Taken    base.Pai
+	Consumed [2]base.Pai
+}
+
+func NewPon(actor int, target int, taken base.Pai, consumed [2]base.Pai, log string) (*Pon, error) {
+	event := &Pon{
+		Actor:    actor,
+		Target:   target,
+		Taken:    taken,
+		Consumed: consumed,
+	}
+
+	if event.Actor == event.Target {
+		return nil, fmt.Errorf("actor and target cannot be the same: %d", event.Actor)
+	}
+
+	isValidPais := !slices.ContainsFunc(event.Consumed[:], func(p base.Pai) bool {
+		return !event.Taken.HasSameSymbol(&p)
+	})
+	if !isValidPais {
+		return nil, fmt.Errorf("taken tile must be the same as the consumed tile: %v", event)
+	}
+
+	if event.Taken.IsUnknown() {
+		return nil, fmt.Errorf("pon tiles must not be unknown: %v", event)
+	}
+
+	if err := eventValidator.Struct(event); err != nil {
+		return nil, err
+	}
+	return event, nil
+}
---
+++ internal/game/event/inbound/pon_test.go
@@ -0,0 +1,184 @@
+package inbound
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
+
+func TestNewPon(t *testing.T) {
+	type args struct {
+		actor    int
+		target   int
+		taken    base.Pai
+		consumed [2]base.Pai
+		log      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Pon
+		wantErr bool
+	}{
+		{
+			name: "valid",
+			args: args{
+				actor:    1,
+				target:   0,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "test",
+			},
+			want: &Pon{
+				Actor:    1,
+				Target:   0,
+				Taken:    *mustPai("6s"),
+				Consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid actor min",
+			args: args{
+				actor:    -1,
+				target:   0,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid actor max",
+			args: args{
+				actor:    4,
+				target:   3,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid target min",
+			args: args{
+				actor:    0,
+				target:   -1,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid target max",
+			args: args{
+				actor:    0,
+				target:   4,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "actor and target are the same",
+			args: args{
+				actor:    0,
+				target:   0,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("6s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "unknown tile",
+			args: args{
+				actor:    0,
+				target:   1,
+				taken:    *mustPai("?"),
+				consumed: [2]base.Pai{*mustPai("?"), *mustPai("?")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid taken tile",
+			args: args{
+				actor:    0,
+				target:   1,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("7s"), *mustPai("7s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "invalid consumed tiles",
+			args: args{
+				actor:    2,
+				target:   3,
+				taken:    *mustPai("6s"),
+				consumed: [2]base.Pai{*mustPai("6s"), *mustPai("7s")},
+				log:      "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "red tile in taken",
+			args: args{
+				actor:    2,
+				target:   3,
+				taken:    *mustPai("5sr"),
+				consumed: [2]base.Pai{*mustPai("5s"), *mustPai("5s")},
+				log:      "",
+			},
+			want: &Pon{
+				Actor:    2,
+				Target:   3,
+				Taken:    *mustPai("5sr"),
+				Consumed: [2]base.Pai{*mustPai("5s"), *mustPai("5s")},
+			},
+			wantErr: false,
+		},
+		{
+			name: "red tile in consumed",
+			args: args{
+				actor:    2,
+				target:   3,
+				taken:    *mustPai("5s"),
+				consumed: [2]base.Pai{*mustPai("5s"), *mustPai("5sr")},
+				log:      "",
+			},
+			want: &Pon{
+				Actor:    2,
+				Target:   3,
+				Taken:    *mustPai("5s"),
+				Consumed: [2]base.Pai{*mustPai("5s"), *mustPai("5sr")},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPon(tt.args.actor, tt.args.target, tt.args.taken, tt.args.consumed, tt.args.log)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPon() error = %v, want %v", err, tt.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPon() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
