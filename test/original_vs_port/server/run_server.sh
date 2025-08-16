2e96b001fff16a8f67b4d051c9538ecc3b9b6e1f
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-16 04:10:28 +0000

    test: Fix bug in run_server.sh

--- test/original_vs_port/server/run_server.sh
+++ test/original_vs_port/server/run_server.sh
@@ -7,18 +7,12 @@ NUM_GAMES=${NUM_GAMES:-1}
 
 GAME_TYPE=${GAME_TYPE:-tonnan}
 
-for i in $(seq 1 "$NUM_GAMES"); do
-    echo "$i/$NUM_GAMES games"
-
-    mjai server \
-        --host=0.0.0.0 \
-        --port=$PORT \
-        --game_type=$GAME_TYPE \
-        --room=$ROOM \
-        --games=$NUM_GAMES \
-        --log_dir="/log_dir"
-
-    echo "finished game $i"
-done
+mjai server \
+    --host=$HOST \
+    --port=$PORT \
+    --game_type=$GAME_TYPE \
+    --room=$ROOM \
+    --games=$NUM_GAMES \
+    --log_dir="/log_dir"
 
 echo "all $NUM_GAMES games done"
