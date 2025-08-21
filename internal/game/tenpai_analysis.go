18d392555113c87c817d308cc9d0afdcb4a14153
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-21 16:49:42 +0000

    feat: Add IsTenpaiAll()

--- internal/game/tenpai_analysis.go
+++ internal/game/tenpai_analysis.go
@@ -6,3 +6,31 @@ func IsTenpaiGeneral(ps *base.PaiSet) (bool, error) {
 	shanten, _, err := AnalyzeShantenWithOption(ps, 0, 0)
 	return shanten <= 0, err
 }
+
+func IsTenpaiAll(ps *base.PaiSet) (bool, error) {
+	shanten, _, err := AnalyzeShantenWithOption(ps, 0, 0)
+	if err != nil {
+		return false, err
+	}
+	if shanten <= 0 {
+		return true, nil
+	}
+
+	shanten, err = AnalyzeShantenChitoitsu(ps)
+	if err != nil {
+		return false, err
+	}
+	if shanten <= 0 {
+		return true, nil
+	}
+
+	shanten, err = AnalyzeShantenKokushimuso(ps)
+	if err != nil {
+		return false, err
+	}
+	if shanten <= 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
--- internal/game/tenpai_analysis_test.go
+++ internal/game/tenpai_analysis_test.go
@@ -80,3 +80,78 @@ func TestIsTenpaiGeneral(t *testing.T) {
 		})
 	}
 }
+
+func TestIsTenpaiAll(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "empty : An empty hand is one step away from being a pair wait -> noten",
+			input:   "",
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "chitoitsu",
+			input:   "1m 1m 8m 8m 2p 8p 8p 5s 5s E E C C",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "thirteen orphans",
+			input:   "1m 9m 1p 9p 1s 9s E S W N P F C",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "tenpai",
+			input:   "1m 2m 3m 4p 5pr 6p 7s 8s 9s E E S S",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "win",
+			input:   "1m 2m 3m 4p 5pr 6p 7s 8s 9s E E S S S",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "with meld",
+			input:   "1m 2m 3m 4p 5pr 6p 7s 8s 9s E",
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "5 identical tiles",
+			input:   "1m 1m 1m 1m 1m",
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pais, err := base.StrToPais(tt.input)
+			if err != nil {
+				t.Errorf("StrToPais() error = %v", err)
+				return
+			}
+			paiSet, err := base.NewPaiSet(pais)
+			if err != nil {
+				t.Errorf("NewPaiSet() error = %v", err)
+				return
+			}
+
+			got, err := IsTenpaiAll(paiSet)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IsTenpaiAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("IsTenpaiAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
