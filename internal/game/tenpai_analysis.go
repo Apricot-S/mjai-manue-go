da66c8169bae9e1aeac787cccb5c60b214c41935
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-23 04:23:07 +0000

    feat: Add GetWaitedPaisAll()

--- internal/game/tenpai_analysis.go
+++ internal/game/tenpai_analysis.go
@@ -1,6 +1,10 @@
 package game
 
-import "github.com/Apricot-S/mjai-manue-go/internal/base"
+import (
+	"fmt"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/base"
+)
 
 func IsTenpaiGeneral(ps *base.PaiSet) (bool, error) {
 	shanten, _, err := AnalyzeShantenWithOption(ps, 0, 0)
@@ -33,4 +37,42 @@ func IsTenpaiAll(ps *base.PaiSet) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func GetWaitedPaisAll(ps *base.PaiSet) (*base.PaiSet, error) {
+	numPais, err := countPais(ps)
+	if err != nil {
+		return nil, err
+	}
+	if (numPais % 3) != 1 {
+		return nil, fmt.Errorf("the waited tiles cannot be calculated if there is a tsumo tile")
+	}
+
+	isTenpai, err := IsTenpaiAll(ps)
+	if err != nil {
+		return nil, err
+	}
+	if !isTenpai {
+		return nil, nil
+	}
+
+	waited, _ := base.NewPaiSet(nil)
+	for i, c := range ps {
+		if c >= 4 {
+			continue
+		}
+
+		ps[i]++
+		isHora, err := IsHoraForm(ps)
+		ps[i]--
+		if err != nil {
+			return nil, err
+		}
+
+		if isHora {
+			waited[i] = 1
+		}
+	}
+
+	return waited, nil
 }
--- internal/game/tenpai_analysis_test.go
+++ internal/game/tenpai_analysis_test.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/Apricot-S/mjai-manue-go/internal/base"
@@ -151,6 +152,91 @@ func TestIsTenpaiAll(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("IsTenpaiAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetWaitedPaisAll(t *testing.T) {
+	tests := []struct {
+		name    string
+		tehai   string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "tenpai",
+			tehai:   "1m 2m 3m 4p 5pr 6p 7s 8s 9s E E S S",
+			want:    "E S",
+			wantErr: false,
+		},
+		{
+			name:    "chitoitsu",
+			tehai:   "1m 1m 8m 8m 2p 8p 8p 5s 5s E E C C",
+			want:    "2p",
+			wantErr: false,
+		},
+		{
+			name:    "thirteen orphans",
+			tehai:   "9m 9m 1p 9p 1s 9s E S W N P F C",
+			want:    "1m",
+			wantErr: false,
+		},
+		{
+			name:    "thirteen orphans 13 waits",
+			tehai:   "1m 9m 1p 9p 1s 9s E S W N P F C",
+			want:    "1m 9m 1p 9p 1s 9s E S W N P F C",
+			wantErr: false,
+		},
+		{
+			name:    "An empty hand does not have tusmo tile",
+			tehai:   "",
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name:    "win",
+			tehai:   "1m 2m 3m 4p 5pr 6p 7s 8s 9s E E S S S",
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tehaiPais, err := base.StrToPais(tt.tehai)
+			if err != nil {
+				t.Errorf("StrToPais() error = %v", err)
+				return
+			}
+			tehaiPaiSet, err := base.NewPaiSet(tehaiPais)
+			if err != nil {
+				t.Errorf("NewPaiSet() error = %v", err)
+				return
+			}
+
+			var waited *base.PaiSet
+			if tt.want == "" {
+				waited = nil
+			} else {
+				waitedPais, err := base.StrToPais(tt.want)
+				if err != nil {
+					t.Errorf("StrToPais() error = %v", err)
+					return
+				}
+				waited, err = base.NewPaiSet(waitedPais)
+				if err != nil {
+					t.Errorf("NewPaiSet() error = %v", err)
+					return
+				}
+			}
+
+			got, err := GetWaitedPaisAll(tehaiPaiSet)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWaitedPaisAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, waited) {
+				t.Errorf("GetWaitedPaisAll() = %v, want %v", got, tt.want)
 			}
 		})
 	}
