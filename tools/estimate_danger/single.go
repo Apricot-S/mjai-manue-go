66595558833a0d15aa5175b4743f373808f82da9
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-28 14:13:59 +0000

    feat: Develop single (WIP)

--- tools/estimate_danger/single.go
+++ tools/estimate_danger/single.go
@@ -16,8 +16,21 @@ func getCriteria() []map[string]bool {
 	return criteria
 }
 
-func calculateProbabilities(r io.Reader, w io.Writer, criteria []map[string]bool) error {
-	return nil
+func createKyokuProbsMap(r io.Reader, w io.Writer, criteria []map[string]bool) (any, error) {
+	return nil, nil
+}
+
+func aggregateProbabilities(w io.Writer, kyokuProbsMap any, criteria []map[string]bool) (any, error) {
+	return nil, nil
+}
+
+func calculateProbabilities(r io.Reader, w io.Writer, criteria []map[string]bool) (any, error) {
+	kyokuProbsMap, err := createKyokuProbsMap(r, w, criteria)
+	if err != nil {
+		return nil, err
+	}
+
+	return aggregateProbabilities(w, kyokuProbsMap, criteria)
 }
 
 func CalculateSingleProbabilities(featuresPath string, w io.Writer) error {
@@ -27,5 +40,8 @@ func CalculateSingleProbabilities(featuresPath string, w io.Writer) error {
 	}
 	defer r.Close()
 
-	return calculateProbabilities(r, w, getCriteria())
+	if _, err := calculateProbabilities(r, w, getCriteria()); err != nil {
+		return err
+	}
+	return nil
 }
