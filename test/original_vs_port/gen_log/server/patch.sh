570fe5b6cd57902a8a1fde9737f164fc762f63d3
Apricot-S <mahjong_apricot@outlook.jp>
2025-08-30 03:20:29 +0000

    fix: Replace deprecated URI.decode with URI.decode_www_form_component

--- test/original_vs_port/gen_log/server/patch.sh
+++ test/original_vs_port/gen_log/server/patch.sh
@@ -5,3 +5,6 @@ sed -i 's/s\.add_dependency("json", \["\([0-9.]*\)"\])/s.add_dependency("json", 
 sed -i 's/s\.add_dependency("nokogiri", \["\([0-9.]*\)"\])/s.add_dependency("nokogiri", ">= \1")/' mjai.gemspec
 sed -i 's/s\.add_dependency("bundler", \["\([0-9.]*\)"\])/s.add_dependency("bundler", ">= \1")/' mjai.gemspec
 sed -i 's/s\.add_dependency("sass", \["\([0-9.]*\)"\])/s.add_dependency("sass", ">= \1")/' mjai.gemspec
+
+# Replace removed URI.decode with URI.decode_www_form_component
+sed -i 's/URI\.decode/URI\.decode_www_form_component/' lib/mjai/tenhou_archive.rb
