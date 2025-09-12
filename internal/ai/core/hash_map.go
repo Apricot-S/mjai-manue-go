83f04f255fff7dd5db4b4fe254faeeced4faa6aa
Apricot-S <mahjong_apricot@outlook.jp>
2025-09-12 14:33:23 +0000

    refactor: Use the Key directly as a map key

--- internal/ai/core/hash_map.go
+++ internal/ai/core/hash_map.go
@@ -1,54 +1,33 @@
 package core
 
-import "encoding/json/v2"
-
 type HashMapKey interface {
 	float64 | [4]float64
 }
 
-type HashMapEntry[T HashMapKey] struct {
-	Key   T
-	Value float64
-}
-
-type HashMap[T HashMapKey] map[string]HashMapEntry[T]
+type HashMap[T HashMapKey] map[T]float64
 
 func NewHashMap[T HashMapKey]() HashMap[T] {
-	return make(map[string]HashMapEntry[T])
-}
-
-func keyToString[T HashMapKey](key T) string {
-	jsonKey, err := json.Marshal(&key)
-	if err != nil {
-		panic(err)
-	}
-	return string(jsonKey)
+	return make(map[T]float64)
 }
 
 func (h *HashMap[T]) Set(key T, value float64) {
-	keyStr := keyToString(key)
-	(*h)[keyStr] = HashMapEntry[T]{
-		Key:   key,
-		Value: value,
-	}
+	(*h)[key] = value
 }
 
 func (h *HashMap[T]) Get(key T, def float64) float64 {
-	keyStr := keyToString(key)
-	if entry, ok := (*h)[keyStr]; ok {
-		return entry.Value
+	if v, ok := (*h)[key]; ok {
+		return v
 	}
 	return def
 }
 
 func (h *HashMap[T]) HasKey(key T) bool {
-	keyStr := keyToString(key)
-	_, ok := (*h)[keyStr]
+	_, ok := (*h)[key]
 	return ok
 }
 
 func (h *HashMap[T]) ForEach(callback func(key T, value float64)) {
-	for _, entry := range *h {
-		callback(entry.Key, entry.Value)
+	for k, v := range *h {
+		callback(k, v)
 	}
 }
