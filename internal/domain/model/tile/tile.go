180961a4618e256ef6c65287e6f15170835c5d0a
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-02 20:46:22 +0000

    refactor: Add and rename constants

--- internal/domain/model/tile/tile.go
+++ internal/domain/model/tile/tile.go
@@ -5,9 +5,16 @@ import (
 	"slices"
 )
 
-const NumTileType = 3*9 + 4 + 3 + 3 + 1
+// NumTileType34 is the number of distinct tile types without red fives.
+const NumTileType34 = 3*9 + 4 + 3
 
-var tileCodes = [NumTileType]string{
+// NumTileType37 is the number of distinct tile types with red fives.
+const NumTileType37 = NumTileType34 + 3
+
+// NumTileType38 is the number of distinct tile types with red fives and unknown tile (?).
+const NumTileType38 = NumTileType37 + 1
+
+var tileCodes = [NumTileType38]string{
 	"1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", // m
 	"1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", // p
 	"1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", // s
