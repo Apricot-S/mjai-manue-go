9a2f64b774e448431ed6811e35627f3bdb4ede72
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-05 01:14:07 +0000

    feat: Add Pair (WIP)

---
+++ internal/domain/model/block/block.go
@@ -0,0 +1,36 @@
+package block
+
+import (
+	"fmt"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+)
+
+type BlockType int
+
+// const (
+// 	Sequence BlockType = iota + 1
+// 	Triplet
+// 	Quad
+// 	Pair
+// )
+
+type Block interface {
+	ToTiles() []tile.Tile
+	ToString() string
+}
+
+type Pair struct {
+	tiles []tile.Tile
+}
+
+func NewPair(t tile.Tile) (*Pair, error) {
+	if t.IsUnknown() {
+		return nil, fmt.Errorf("cannot create pair from unknown tile")
+	}
+	return &Pair{tiles: []tile.Tile{t, t}}, nil
+}
+
+func (p *Pair) ToTiles() []tile.Tile {
+	return p.tiles
+}
