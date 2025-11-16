7065870da434b348c72ccf7f606218794df8a5df
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-16 23:50:27 +0000

    feat: Implement Meld (WIP)

--- internal/domain/model/meld/meld.go
+++ internal/domain/model/meld/meld.go
@@ -2,8 +2,6 @@ package meld
 
 import (
 	"fmt"
-	"slices"
-	"sort"
 	"strings"
 
 	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/block"
@@ -40,65 +38,4 @@ func meldToString(m Meld) string {
 	}
 
 	return fmt.Sprintf("[%s(%d)/%s]", m.Taken().Code(), m.Target(), strings.Join(consumedStrs, " "))
-}
-
-type Pon struct {
-	taken    tile.Tile
-	consumed [2]tile.Tile
-	target   int
-	tiles    []tile.Tile
-}
-
-func NewPon(taken tile.Tile, consumed [2]tile.Tile, target int) (*Pon, error) {
-	if !isValidTarget(target) {
-		return nil, fmt.Errorf("invalid target: %d", target)
-	}
-
-	tiles := tile.Tiles{taken, consumed[0], consumed[1]}
-	if slices.ContainsFunc(tiles, func(t tile.Tile) bool { return t.IsUnknown() }) {
-		return nil, fmt.Errorf("unknown tile cannot use for Pon")
-	}
-	if slices.ContainsFunc(tiles, func(t tile.Tile) bool { return !taken.HasSameSymbol(&t) }) {
-		return nil, fmt.Errorf("mismatch taken: %+v, consumed: %+v", taken, consumed)
-	}
-	if countRed(tiles) > 1 {
-		return nil, fmt.Errorf("cannot use 2 or more red fives for Pon; taken: %+v, consumed: %+v", taken, consumed)
-	}
-
-	sort.Sort(tiles)
-
-	csm := tile.Tiles(consumed[:])
-	sort.Sort(csm)
-
-	return &Pon{
-		taken:    taken,
-		consumed: [2]tile.Tile(csm),
-		target:   target,
-		tiles:    tiles,
-	}, nil
-}
-
-func (p *Pon) Taken() *tile.Tile {
-	return &p.taken
-}
-
-func (p *Pon) Consumed() []tile.Tile {
-	return p.consumed[:]
-}
-
-func (p *Pon) Target() int {
-	return p.target
-}
-
-func (p *Pon) ToTiles() []tile.Tile {
-	return p.tiles
-}
-
-func (p *Pon) ToBlock() block.Block {
-	// Red five is sorted after normal, so RemoveRed() is not necessary.
-	return block.MustTriplet(p.tiles[0])
-}
-
-func (p *Pon) ToString() string {
-	return meldToString(p)
 }
