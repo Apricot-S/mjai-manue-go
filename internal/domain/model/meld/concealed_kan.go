296550bc5828af2e01255b0314b5792d6b5caf7a
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-17 01:14:24 +0000

    feat: Implement ConcealedKan (WIP)

--- internal/domain/model/meld/concealed_kan.go
+++ internal/domain/model/meld/concealed_kan.go
@@ -1,1 +1,39 @@
 package meld
+
+import (
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/block"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+)
+
+type ConcealedKan struct {
+	consumed [4]tile.Tile
+}
+
+func NewConcealedKan(consumed [4]tile.Tile) (*ConcealedKan, error) {
+	panic("")
+}
+
+func (k *ConcealedKan) Taken() *tile.Tile {
+	panic("")
+}
+
+func (k *ConcealedKan) Consumed() []tile.Tile {
+	return k.consumed[:]
+}
+
+func (k *ConcealedKan) Target() int {
+	panic("")
+}
+
+func (k *ConcealedKan) ToTiles() []tile.Tile {
+	return k.consumed[:]
+}
+
+func (k *ConcealedKan) ToBlock() block.Block {
+	// Red five is sorted after normal, so RemoveRed() is not necessary.
+	return block.MustQuad(k.consumed[0])
+}
+
+func (k *ConcealedKan) ToString() string {
+	panic("")
+}
