2c5a0d3839452e03e93e3ca0b059f33f8de952bb
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-23 09:53:55 +0000

    feat: Add PlayerID

---
+++ internal/domain/model/playerid/playerid.go
@@ -0,0 +1,27 @@
+package playerid
+
+import "fmt"
+
+type PlayerID struct {
+	value int
+}
+
+func NewPlayerID(id int) (*PlayerID, error) {
+	if id < 0 || 3 < id {
+		return nil, fmt.Errorf("invalid player id: %d", id)
+	}
+
+	return &PlayerID{value: id}, nil
+}
+
+func MustPlayerID(id int) *PlayerID {
+	pid, err := NewPlayerID(id)
+	if err != nil {
+		panic(err)
+	}
+	return pid
+}
+
+func (id *PlayerID) Index() int {
+	return id.value
+}
---
+++ internal/domain/model/playerid/playerid_test.go
@@ -0,0 +1,58 @@
+package playerid_test
+
+import (
+	"testing"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/playerid"
+)
+
+func TestNewPlayerID(t *testing.T) {
+	tests := []struct {
+		name      string
+		id        int
+		wantIndex int
+		wantErr   bool
+	}{
+		{
+			name:      "valid ID: 0",
+			id:        0,
+			wantIndex: 0,
+			wantErr:   false,
+		},
+		{
+			name:      "valid ID: 3",
+			id:        3,
+			wantIndex: 3,
+			wantErr:   false,
+		},
+		{
+			name:      "invalid ID: -1",
+			id:        -1,
+			wantIndex: -1,
+			wantErr:   true,
+		},
+		{
+			name:      "invalid ID: 4",
+			id:        4,
+			wantIndex: 4,
+			wantErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := playerid.NewPlayerID(tt.id)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("NewPlayerID() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("NewPlayerID() succeeded unexpectedly")
+			}
+			if got.Index() != tt.wantIndex {
+				t.Errorf("NewPlayerID().Index() = %v, want %v", got, tt.wantIndex)
+			}
+		})
+	}
+}
