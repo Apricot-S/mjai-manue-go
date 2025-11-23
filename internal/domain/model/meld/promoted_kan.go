c837db25fdfccd82a1d701799238291cea79ea0e
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-23 10:55:31 +0000

    feat: Implement PromotedKan (WIP)

--- internal/domain/model/meld/promoted_kan.go
+++ internal/domain/model/meld/promoted_kan.go
@@ -1,6 +1,10 @@
 package meld
 
 import (
+	"fmt"
+	"slices"
+	"sort"
+
 	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/playerid"
 	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
 )
@@ -19,12 +23,28 @@ func NewPromotedKan(
 	added tile.Tile,
 	target playerid.PlayerID,
 ) (*PromotedKan, error) {
+	tiles := tile.Tiles{taken, consumed[0], consumed[1], added}
+	if slices.ContainsFunc(tiles, func(t tile.Tile) bool { return t.IsUnknown() }) {
+		return nil, fmt.Errorf("unknown tile cannot use for Promoted Kan")
+	}
+	if slices.ContainsFunc(tiles, func(t tile.Tile) bool { return !taken.HasSameSymbol(&t) }) {
+		return nil, fmt.Errorf("mismatch taken: %+v, consumed: %+v, added: %+v", taken, consumed, added)
+	}
+	if taken.IsSuits() && taken.Number() == 5 && countRed(tiles) != 1 {
+		return nil, fmt.Errorf("must contain a red five for Promoted Kan of 5; taken: %+v, consumed: %+v, added: %+v", taken, consumed, added)
+	}
+
+	sort.Sort(tiles)
+
+	csm := tile.Tiles(consumed[:])
+	sort.Sort(csm)
+
 	return &PromotedKan{
 		taken:    taken,
-		consumed: consumed,
+		consumed: [2]tile.Tile(csm),
 		added:    added,
 		target:   target,
-		tiles:    []tile.Tile{},
+		tiles:    tiles,
 	}, nil
 }
 
