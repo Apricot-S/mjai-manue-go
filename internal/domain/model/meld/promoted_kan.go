ab71fe04ab761b8631afa51a042dcc5467a652c1
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-23 07:39:02 +0000

    feat: Implement PromotedKan (WIP)

--- internal/domain/model/meld/promoted_kan.go
+++ internal/domain/model/meld/promoted_kan.go
@@ -1,1 +1,37 @@
 package meld
+
+import "github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+
+type PromotedKan struct {
+	taken    tile.Tile
+	consumed [2]tile.Tile
+	added    tile.Tile
+	target   int
+	tiles    []tile.Tile
+}
+
+func NewPromotedKan(taken tile.Tile, consumed [2]tile.Tile, added tile.Tile, target int) (*PromotedKan, error) {
+	return &PromotedKan{
+		taken:    taken,
+		consumed: consumed,
+		added:    added,
+		target:   target,
+		tiles:    []tile.Tile{},
+	}, nil
+}
+
+func (k *PromotedKan) Taken() *tile.Tile {
+	return &k.taken
+}
+
+func (k *PromotedKan) Consumed() []tile.Tile {
+	return k.consumed[:]
+}
+
+func (k *PromotedKan) Added() *tile.Tile {
+	return &k.added
+}
+
+func (k *PromotedKan) Target() int {
+	return k.target
+}
---
+++ internal/domain/model/meld/promoted_kan_test.go
@@ -0,0 +1,63 @@
+package meld_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/meld"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+)
+
+func TestNewPromotedKan(t *testing.T) {
+	tests := []struct {
+		name         string
+		taken        tile.Tile
+		consumed     [2]tile.Tile
+		added        tile.Tile
+		target       int
+		wantTaken    *tile.Tile
+		wantConsumed []tile.Tile
+		wantAdded    *tile.Tile
+		wantTarget   int
+		wantErr      bool
+	}{
+		{
+			name:         "valid target: 0",
+			taken:        *tile.MustTileFromCode("1p"),
+			consumed:     [2]tile.Tile{*tile.MustTileFromCode("1p"), *tile.MustTileFromCode("1p")},
+			added:        *tile.MustTileFromCode("1p"),
+			target:       0,
+			wantTaken:    tile.MustTileFromCode("1p"),
+			wantConsumed: []tile.Tile{*tile.MustTileFromCode("1p"), *tile.MustTileFromCode("1p")},
+			wantAdded:    tile.MustTileFromCode("1p"),
+			wantTarget:   0,
+			wantErr:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := meld.NewPromotedKan(tt.taken, tt.consumed, tt.added, tt.target)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("NewPromotedKan() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("NewPromotedKan() succeeded unexpectedly")
+			}
+			if !reflect.DeepEqual(got.Taken(), tt.wantTaken) {
+				t.Errorf("NewPromotedKan().Taken() = %v, want %v", got.Taken(), tt.wantTaken)
+			}
+			if !reflect.DeepEqual(got.Consumed(), tt.wantConsumed) {
+				t.Errorf("NewPromotedKan().Consumed() = %v, want %v", got.Consumed(), tt.wantConsumed)
+			}
+			if !reflect.DeepEqual(got.Added(), tt.wantAdded) {
+				t.Errorf("NewPromotedKan().Added() = %v, want %v", got.Added(), tt.wantAdded)
+			}
+			if !reflect.DeepEqual(got.Target(), tt.wantTarget) {
+				t.Errorf("NewPromotedKan().Target() = %v, want %v", got.Target(), tt.wantTarget)
+			}
+		})
+	}
+}
