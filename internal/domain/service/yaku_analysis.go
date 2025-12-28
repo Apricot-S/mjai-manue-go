c5500ec509c46c6ec45628b584005c6f71917719
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-28 14:23:46 +0000

    feat: Add CalculateFuHan (WIP)

---
+++ internal/domain/service/yaku_analysis.go
@@ -0,0 +1,95 @@
+package service
+
+import (
+	"slices"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/block"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/hand"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/meld"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+)
+
+type WinEvent int
+
+const (
+	NoEvent WinEvent = iota + 1
+	RobbingAKan
+	AfterAKan
+	LastTile
+	FirstTile
+)
+
+// Note:
+// This function calculates Fu and Han based on the original mjai-manue specification,
+// not the exact Fu and Han.
+func CalculateFuHan(
+	hand *hand.VisibleHand,
+	handBlocks []block.Block,
+	melds []meld.Meld,
+	prevalentWind *tile.Tile,
+	seatWind *tile.Tile,
+	doraIndicators []tile.Tile,
+	tsumo bool,
+	riichi bool,
+) (fu int, han int, yakus map[string]int) {
+	meldBlocks := make([]block.Block, len(melds))
+	for i, m := range melds {
+		meldBlocks[i] = m.ToBlock()
+	}
+
+	allBlocks := slices.Concat(handBlocks, meldBlocks)
+
+	allTiles := make(tile.Tiles, 0, 14)
+	for _, b := range allBlocks {
+		allTiles = append(allTiles, b.ToTiles()...)
+	}
+
+	hasMeld := len(melds) > 0
+
+	han = 0
+	yakus = make(map[string]int)
+
+	addYaku := func(existsYaku bool, name string, menzenFan int, kuiFan int) {
+		if !existsYaku {
+			return
+		}
+
+		var yakuFan int
+		if hasMeld {
+			yakuFan = kuiFan
+		} else {
+			yakuFan = menzenFan
+		}
+
+		if yakuFan > 0 {
+			yakus[name] = yakuFan
+			han += yakuFan
+		}
+	}
+
+	addYaku(riichi, "reach", 1, 0)
+
+	pinfu := false
+
+	// TODO Calculate fu more accurately
+	if pinfu || hasMeld {
+		fu = 30
+	} else {
+		fu = 40
+	}
+
+	return fu, han, yakus
+}
+
+func Has1Han(
+	hand *hand.VisibleHand,
+	melds []meld.Meld,
+	winningTile *tile.Tile,
+	prevalentWind *tile.Tile,
+	seatWind *tile.Tile,
+	tsumo bool,
+	riichi bool,
+	event WinEvent,
+) bool {
+	return false
+}
---
+++ internal/domain/service/yaku_analysis_test.go
@@ -0,0 +1,85 @@
+package service_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/block"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/hand"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/meld"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/model/tile"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/service"
+)
+
+func TestCalculateFuHan(t *testing.T) {
+	tests := []struct {
+		name           string
+		handCodes      []string
+		handBlocks     []block.Block
+		melds          []meld.Meld
+		prevalentWind  *tile.Tile
+		seatWind       *tile.Tile
+		doraIndicators []tile.Tile
+		tsumo          bool
+		riichi         bool
+		wantFu         int
+		wantHan        int
+		wantYakus      map[string]int
+	}{
+		{
+			name:      "no Yaku",
+			handCodes: []string{"1m", "1m", "1m", "2p", "3p", "4p", "3s", "4s", "5s", "6s", "6s", "6s", "9s", "9s"},
+			handBlocks: []block.Block{
+				block.MustTriplet(*tile.MustTileFromCode("1m")),
+				block.MustSequence(*tile.MustTileFromCode("2p")),
+				block.MustSequence(*tile.MustTileFromCode("3s")),
+				block.MustTriplet(*tile.MustTileFromCode("6s")),
+				block.MustPair(*tile.MustTileFromCode("9s")),
+			},
+			melds:          nil,
+			prevalentWind:  tile.MustTileFromCode("E"),
+			seatWind:       tile.MustTileFromCode("N"),
+			doraIndicators: nil,
+			tsumo:          false,
+			riichi:         false,
+			wantFu:         40,
+			wantHan:        0,
+			wantYakus:      map[string]int{},
+		},
+		{
+			name:      "only Riichi",
+			handCodes: []string{"1m", "1m", "1m", "2p", "3p", "4p", "3s", "4s", "5s", "6s", "6s", "6s", "9s", "9s"},
+			handBlocks: []block.Block{
+				block.MustTriplet(*tile.MustTileFromCode("1m")),
+				block.MustSequence(*tile.MustTileFromCode("2p")),
+				block.MustSequence(*tile.MustTileFromCode("3s")),
+				block.MustTriplet(*tile.MustTileFromCode("6s")),
+				block.MustPair(*tile.MustTileFromCode("9s")),
+			},
+			melds:          nil,
+			prevalentWind:  tile.MustTileFromCode("E"),
+			seatWind:       tile.MustTileFromCode("N"),
+			doraIndicators: nil,
+			tsumo:          false,
+			riichi:         true,
+			wantFu:         40,
+			wantHan:        1,
+			wantYakus:      map[string]int{"reach": 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hand := hand.CodesToHand(tt.handCodes)
+			fu, han, yakus := service.CalculateFuHan(hand, tt.handBlocks, tt.melds, tt.prevalentWind, tt.seatWind, tt.doraIndicators, tt.tsumo, tt.riichi)
+			if tt.wantFu != fu {
+				t.Errorf("CalculateFuHan() = %v, want %v", fu, tt.wantFu)
+			}
+			if tt.wantHan != han {
+				t.Errorf("CalculateFuHan() = %v, want %v", han, tt.wantHan)
+			}
+			if !reflect.DeepEqual(tt.wantYakus, yakus) {
+				t.Errorf("CalculateFuHan() = %v, want %v", yakus, tt.wantYakus)
+			}
+		})
+	}
+}
