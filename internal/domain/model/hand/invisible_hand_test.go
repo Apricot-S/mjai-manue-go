5aa0ede47c0004d2b97fac6f26899ed1d40ecea3
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-31 18:48:53 +0000

    feat: Add Hand methods (WIP)

--- internal/domain/model/hand/invisible_hand_test.go
+++ internal/domain/model/hand/invisible_hand_test.go
@@ -263,6 +263,17 @@ func TestInvisibleHand_Call(t *testing.T) {
 			wantTiles: []tile.Tile{},
 			wantErr:   false,
 		},
+		{
+			name:  "called kan",
+			tiles: []tile.Tile{*tile.MustTileFromCode("?"), *tile.MustTileFromCode("?"), *tile.MustTileFromCode("?")},
+			meld: meld.MustCalledKan(
+				*tile.MustTileFromCode("1m"),
+				[3]tile.Tile{*tile.MustTileFromCode("1m"), *tile.MustTileFromCode("1m"), *tile.MustTileFromCode("1m")},
+				*playerid.MustPlayerID(0),
+			),
+			wantTiles: []tile.Tile{},
+			wantErr:   false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
