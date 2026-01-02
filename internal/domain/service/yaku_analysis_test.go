88e387b4b78b200f30287c045df45c61a37da005
Apricot-S <mahjong_apricot@outlook.jp>
2026-01-02 07:43:04 +0000

    feat: Implement Has1Han (WIP)

--- internal/domain/service/yaku_analysis_test.go
+++ internal/domain/service/yaku_analysis_test.go
@@ -713,6 +713,18 @@ func TestHas1Han(t *testing.T) {
 			want:          true,
 		},
 		{
+			name:          "no Yaku",
+			handCodes:     []string{"1m", "1m", "1m", "2p", "3p", "4p", "3s", "4s", "5s", "6s", "6s", "6s", "9s"},
+			melds:         nil,
+			winningTile:   tile.MustTileFromCode("9s"),
+			prevalentWind: wind.East,
+			seatWind:      wind.South,
+			tsumo:         false,
+			riichi:        false,
+			event:         service.NoEvent,
+			want:          false,
+		},
+		{
 			name:          "only Riichi",
 			handCodes:     []string{"1m", "1m", "1m", "2p", "3p", "4p", "3s", "4s", "5s", "6s", "6s", "6s", "9s"},
 			melds:         nil,
