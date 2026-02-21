2507a0d260b9c73207744b4e3e94e0d32fe765cd
Apricot-S <mahjong_apricot@outlook.jp>
2026-02-21 17:28:21 +0000

    fix: Supports Ruby 4.0

--- test/original_vs_port/gen_log/server/patch.sh
+++ test/original_vs_port/gen_log/server/patch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Remove deprecated `s.has_rdoc`
+sed -i 's/s\.has_rdoc = true//' mjai.gemspec
+
 # Replace strict version constraint with a flexible ">= version" requirement
 sed -i 's/s\.add_dependency("json", \["\([0-9.]*\)"\])/s.add_dependency("json", ">= \1")/' mjai.gemspec
 sed -i 's/s\.add_dependency("nokogiri", \["\([0-9.]*\)"\])/s.add_dependency("nokogiri", ">= \1")/' mjai.gemspec
 sed -i 's/s\.add_dependency("bundler", \["\([0-9.]*\)"\])/s.add_dependency("bundler", ">= \1")/' mjai.gemspec
 sed -i 's/s\.add_dependency("sass", \["\([0-9.]*\)"\])/s.add_dependency("sass", ">= \1")/' mjai.gemspec
 
-# Replace removed URI.decode with URI.decode_www_form_component
+# Replace removed `URI.decode` with `URI.decode_www_form_component`
 sed -i 's/URI\.decode/URI\.decode_www_form_component/' lib/mjai/tenhou_archive.rb
