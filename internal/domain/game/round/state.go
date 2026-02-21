d07b8b309fb457fd974e31bc87b39746d17303c9
Apricot-S <mahjong_apricot@outlook.jp>
2026-02-21 13:46:32 +0000

    feat: Add round.State (WIP)

---
+++ internal/domain/game/round/state.go
@@ -0,0 +1,22 @@
+package round
+
+import (
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/game/round/player"
+	"github.com/Apricot-S/mjai-manue-go/internal/domain/game/tile"
+)
+
+const (
+	NumPlayers           = 4
+	MaxNumDoraIndicators = 5
+	NumInitWall          = tile.NumTileType34*4 - 13*NumPlayers - 14
+	FinalTurn            = float64(NumInitWall) / float64(NumPlayers)
+
+	maxNumKan = 4
+	// Indicates that no action has been taken by anyone.
+	noActor                 = -1
+	kanPlayerStatusMultiple = 4
+)
+
+type StateViewer interface {
+	Players() *[NumPlayers]player.Player
+}
