8a6482c1e56e8f6ca20df75b0763ac93f77268ea
Apricot-S <mahjong_apricot@outlook.jp>
2026-03-08 14:17:40 +0000

    feat: Add event (WIP)

--- internal/domain/game/event/end_game.go
+++ internal/domain/game/event/end_game.go
@@ -3,6 +3,10 @@ package event
 type EndGame struct {
 }
 
+func NewEndGame() *EndGame {
+	return &EndGame{}
+}
+
 func (e EndGame) EventType() string {
 	return "end_game"
 }
--- internal/domain/game/event/end_round.go
+++ internal/domain/game/event/end_round.go
@@ -3,6 +3,10 @@ package event
 type EndRound struct {
 }
 
+func NewEndRound() *EndRound {
+	return &EndRound{}
+}
+
 func (e EndRound) EventType() string {
 	return "end_round"
 }
