d5afacfaa4f6242aafa63109e340b92740cc1a30
Apricot-S <mahjong_apricot@outlook.jp>
2026-04-26 12:23:58 +0000

    refactor: Remove unused game.StateViewer

--- internal/domain/game/state.go
+++ internal/domain/game/state.go
@@ -2,10 +2,6 @@ package game
 
 import "github.com/Apricot-S/mjai-manue-go/internal/domain/game/common"
 
-type StateViewer interface {
-	Scores() [common.NumPlayers]int
-}
-
 type State struct {
 	scores [common.NumPlayers]int
 }
