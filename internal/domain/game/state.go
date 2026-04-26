31539b011a6f9e1dd274878d890c4ed147d327c0
Apricot-S <mahjong_apricot@outlook.jp>
2026-04-26 05:41:45 +0000

    feat: Develop game.State

--- internal/domain/game/state.go
+++ internal/domain/game/state.go
@@ -9,3 +9,21 @@ type StateViewer interface {
 type State struct {
 	scores [common.NumPlayers]int
 }
+
+func NewState(scores [common.NumPlayers]int) *State {
+	return &State{
+		scores: scores,
+	}
+}
+
+func NewDefaultState() *State {
+	return NewState([common.NumPlayers]int{25000, 25000, 25000, 25000})
+}
+
+func (s *State) Scores() [common.NumPlayers]int {
+	return s.scores
+}
+
+func (s *State) UpdateScores(scores [common.NumPlayers]int) {
+	s.scores = scores
+}
