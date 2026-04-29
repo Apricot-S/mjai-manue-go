43d3a0b92dfcd84f67888ce40e9752affda93e43
Apricot-S <mahjong_apricot@outlook.jp>
2026-04-29 06:52:29 +0000

    refactor: Move runtime trace helpers out of stdio

--- internal/adapter/mjai/runtime/stdio.go
+++ internal/adapter/mjai/runtime/stdio.go
@@ -6,7 +6,6 @@ import (
 	"io"
 
 	"github.com/Apricot-S/mjai-manue-go/internal/adapter/mjai/inbound"
-	"github.com/Apricot-S/mjai-manue-go/internal/adapter/mjai/outbound"
 	"github.com/Apricot-S/mjai-manue-go/internal/domain/ai"
 )
 
@@ -53,29 +52,4 @@ func RunStdio(cfg StdioConfig) error {
 		return err
 	}
 	return nil
-}
-
-func writeMessageWithTrace(w *bufio.Writer, msg outbound.Message, log io.Writer) error {
-	b, err := outbound.MarshalMessage(msg)
-	if err != nil {
-		return err
-	}
-	if err := traceLine(log, "->", b); err != nil {
-		return err
-	}
-	if _, err := w.Write(b); err != nil {
-		return err
-	}
-	if err := w.WriteByte('\n'); err != nil {
-		return err
-	}
-	return w.Flush()
-}
-
-func traceLine(log io.Writer, direction string, line []byte) error {
-	if log == nil {
-		return nil
-	}
-	_, err := fmt.Fprintf(log, "%s\t%s\n", direction, string(line))
-	return err
 }
