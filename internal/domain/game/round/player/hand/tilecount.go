ab217aa22dfdd9943f62aa9619eac6b72ce02174
Apricot-S <mahjong_apricot@outlook.jp>
2026-04-29 09:34:05 +0000

    refactor: Preallocate tile slices in TileCounts34.ToTiles

--- internal/domain/game/round/player/hand/tilecount.go
+++ internal/domain/game/round/player/hand/tilecount.go
@@ -5,7 +5,7 @@ import "github.com/Apricot-S/mjai-manue-go/internal/domain/game/tile"
 type TileCounts34 [tile.NumTileType34]int
 
 func (tc34 *TileCounts34) ToTiles() []tile.Tile {
-	tiles := make([]tile.Tile, 0)
+	tiles := make([]tile.Tile, 0, tc34.NumTiles())
 	for i, c := range tc34 {
 		for range c {
 			tiles = append(tiles, *tile.MustTileFromID(i))
