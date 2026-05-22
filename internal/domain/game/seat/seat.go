fa24c52f30e75c94d26984fa45f6d7d27030797f
Apricot-S <mahjong_apricot@outlook.jp>
2026-05-22 21:53:19 +0000

    refactor: Rename receivers

--- internal/domain/game/seat/seat.go
+++ internal/domain/game/seat/seat.go
@@ -22,8 +22,8 @@ func MustSeat(id int) Seat {
 	return s
 }
 
-func (seat Seat) Index() int {
-	return seat.index
+func (s Seat) Index() int {
+	return s.index
 }
 
 // DistanceFrom returns seat's relative position from base as a value in 0..3.
@@ -31,11 +31,11 @@ func (seat Seat) Index() int {
 // A return value of 0 means the same seat as base, 1 means shimocha, 2 means
 // toimen, and 3 means kamicha. If base is the starting dealer seat 0, the
 // return value matches seat.Index().
-func (seat Seat) DistanceFrom(base Seat) int {
-	return (seat.index - base.index + 4) % 4
+func (s Seat) DistanceFrom(base Seat) int {
+	return (s.index - base.index + 4) % 4
 }
 
 // IsShimochaOf reports whether seat is the player to the left of target.
-func (seat Seat) IsShimochaOf(target Seat) bool {
-	return seat.index == (target.index+1)%4
+func (s Seat) IsShimochaOf(target Seat) bool {
+	return s.index == (target.index+1)%4
 }
